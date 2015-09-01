f7a6c082764548c76362e5382df0b4b1fe46ef66
Talal Qadri <tqadri@tqadri.aus.csb>
2015-09-01 12:19:22 -0500

    Added multiple rules

--- ba-enablement-language/src/main/java/com/redhat/Reason.java
+++ ba-enablement-language/src/main/java/com/redhat/Reason.java
@@ -3,4 +3,32 @@ package com.redhat;
 public class Reason {
 
 	private String reasonMessage; // tell me what happened? was it null?
+	
+	public Reason(){
+	}
+	
+	public String getReasonMessage() {
+		return reasonMessage;
+	}
+
+	public void setReasonMessage(String reasonMessage) {
+		this.reasonMessage = reasonMessage;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		Reason other = (Reason) obj;
+		if(this.getReasonMessage().equals(other.getReasonMessage())){
+			return true;
+		}
+		
+		else if (this.getReasonMessage() == null && other.getReasonMessage() == null){
+			return true;
+		}
+		
+		System.out.println(reasonMessage);
+		System.out.println("here");
+		System.out.println("Other: " + other.getReasonMessage());
+		return false;
+	}
 }
--- ba-enablement-language/src/main/java/com/redhat/RuleResponse.java
+++ ba-enablement-language/src/main/java/com/redhat/RuleResponse.java
@@ -7,8 +7,10 @@ public class RuleResponse {
 	@KieQuery(binding = "$business", queryName = "Get Business")
 	private Business business;
 	
+	@KieQuery(binding = "$responseCode", queryName = "Get Response Code")
 	private String responseCode; // e.g. success, failure, filtered, validation error...
 	
+	@KieQuery(binding = "$reason", queryName = "Get Reason")
 	private Collection<Reason> reasons; // messages about why the response Code came bacl
 
 	public Business getBusiness() {
@@ -17,6 +19,22 @@ public class RuleResponse {
 
 	public void setBusiness(Business business) {
 		this.business = business;
+	}
+
+	public String getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(String responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	public Collection<Reason> getReasons() {
+		return reasons;
+	}
+
+	public void setReasons(Collection<Reason> reasons) {
+		this.reasons = reasons;
 	}
 
 }
